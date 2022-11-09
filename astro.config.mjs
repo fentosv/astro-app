a98b059ee9e52ccb92f9b3997cffd0fe44584b1f
fentosv <fentos.dev@gmail.com>
2022-11-09 11:50:25 +0100

    Add fetched data page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-import react from "@astrojs/react";
+import react from '@astrojs/react'
+
+// https://astro.build/config
+import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
-});
+  integrations: [
+    react(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+  ],
+})

fa909beb4396b04e19b8d6ad2498d57bf98aa7ab
fentosv <fentos.dev@gmail.com>
2022-11-10 19:13:00 +0100

    Vercel build and some fresh styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,22 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import react from '@astrojs/react'
+import react from '@astrojs/react';
 
 // https://astro.build/config
-import image from '@astrojs/image'
+import image from '@astrojs/image';
 
 // https://astro.build/config
-import preact from '@astrojs/preact'
+import preact from '@astrojs/preact';
+
+// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-    preact(),
-  ],
-})
+  integrations: [react(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), preact()],
+  output: "server",
+  adapter: vercel()
+});
